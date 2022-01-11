43b68eb59271732b716eb123542416576ab7e685
shijianka <shijianka@foxmail.com>
2022-01-11 15:20:15 +0800

    新增:Delete  成员变量   删除文件和文件夹数量   已删除的文件、文件夹的名字的集合

--- privateTools/src/cn/shijianka/file/DeleteFile.java
+++ privateTools/src/cn/shijianka/file/DeleteFile.java
@@ -1,14 +1,32 @@
 package cn.shijianka.file;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class DeleteFile {
-/**
- *   2022年1月11日
- *      --增加删除功能
- */
+    /**
+     * 2022年1月11日
+     * --增加删除功能
+     */
 
     public DeleteFile() {
+    }
+
+    private int deleteForFileCount = 0; //成功删除的文件总数量
+    private int deleteForDirectoryCount = 0; //成功删除的文件夹总数量
+    private ArrayList<String> deleteNameForFileList = new ArrayList<>(); //删除的文件名集合
+    private ArrayList<String> deleteNameForDirectoryList = new ArrayList<>(); //删除的文件夹名称集合
+
+    public int getDeleteForFileCount() {
+        return deleteForFileCount;
+    }
+
+    public ArrayList<String> getDeleteNameForFileList() {
+        return deleteNameForFileList;
+    }
+
+    public ArrayList<String> getDeleteNameForDirectoryList() {
+        return deleteNameForDirectoryList;
     }
 
     /**
@@ -31,9 +49,13 @@ public class DeleteFile {
      */
     public boolean deleteFile(String targetFile) {
         File file = new File(targetFile);
-        if(file.exists()&&file.isFile()){
-            file.delete();
-            return true;
+        if (file.exists() && file.isFile()) {
+            boolean delete = file.delete();
+            if (delete) {
+                deleteForFileCount++;
+                deleteNameForFileList.add(file.getName());
+            }
+            return delete;
         }
         return false;
     }
@@ -42,30 +64,36 @@ public class DeleteFile {
      * 删除目标文件夹
      */
     public boolean deleteDirectory(String targetDirectory) {
-       if(!targetDirectory.endsWith("\\")){
-           targetDirectory +="\\";
-       }
-       File file = new File(targetDirectory);
-       if(!file.exists()||!file.isDirectory()){
-           return false;
-       }
+        if (!targetDirectory.endsWith("\\")) {
+            targetDirectory += "\\";
+        }
+        File file = new File(targetDirectory);
+        if (!file.exists() || !file.isDirectory()) {
+            return false;
+        }
         File[] files = file.listFiles();
         for (int i = 0; i < files.length; i++) {
-            if(files[i].isFile()){
+            if (files[i].isFile()) {
                 boolean delete = files[i].delete();
-                if(!delete){
+                if (delete) {
+                    deleteForFileCount++;
+                    deleteNameForFileList.add(files[i].getName());
+                }
+                if (!delete) {
                     break;
                 }
-            }else {
+            } else {
                 boolean b = deleteDirectory(files[i].getAbsolutePath());
-                if(!b){
+                if (!b) {
                     break;
                 }
             }
         }
-        if(file.delete()){
+        if (file.delete()) {
+            deleteForDirectoryCount++;
+            deleteNameForDirectoryList.add(file.getName());
             return true;
-        }else {
+        } else {
             return false;
         }
     }
